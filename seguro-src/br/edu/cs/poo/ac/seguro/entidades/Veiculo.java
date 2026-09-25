2f70ba61665135c2eb2368eb7d341506bec73670
Marcus Barbosa <mvpb@cesar.school>
2026-09-24 21:41:46 -0300

    DAOs implementados e testados

---
+++ seguro-src/br/edu/cs/poo/ac/seguro/entidades/Veiculo.java
@@ -0,0 +1,18 @@
+package br.edu.cs.poo.ac.seguro.entidades;
+
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Veiculo implements Serializable {
+
+    private String placa;
+    private int ano;
+    private SeguradoEmpresa proprietarioEmpresa;
+    private SeguradoPessoa proprietarioPessoa;
+    private CategoriaVeiculo categoria;
+}
